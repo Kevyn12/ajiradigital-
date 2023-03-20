1f2d545e11ee8a757ffd23ae30f6066dc3b8358d
kevyn12 <kevyn.wafula@gmail.com>
2023-03-20 06:25:36 +0300

    changes

--- connect.php
+++ connect.php
@@ -1,26 +1,22 @@
 <?php
-$Firstname = $_POST['Firstname'];
-$Lastname = $_POST['Lastname'];
-$Email  = $_POST['Email'];
-$Password = $_POST['Password'];
+if(isset($_POST['Firstname']) || isset($_POST['Lastname']) || isset($_POST['Email']) || isset($_POST['Password']))
 
-if(isset($Firstname) || isset($Lastname) || isset($Email) || isset($Password))
-{
-	// rest of the code
-}
+    $Firstname = $_POST['Firstname'];
+    $Lastname = $_POST['Lastname'];
+    $Email  = $_POST['Email'];
+    $Password = $_POST['Password'];
 
-//Database connection
-$conn = new mysqli('localhost','root','','Ajiradigital-');
-if($conn->connect_error){
-	die('connection failed :'.$conn-conect_error);
-}else{
-	$stmt = $conn->prepare("insert into ajiradb(Firstname, Lastname, Email, Password)
-	values(?, ?, ?, ?)");
-	$stmt->bind_param("ssss",$Firstname, $Lastname, $Email, $Password);
-	$stmt->execute();
-	echo"registration successful";
-	$stmt->close();
-	$conn->close();
-}
-
+    //Database connection
+    $conn = new mysqli('localhost','root','','Ajiradigital-');
+    if($conn->connect_error){
+        die('connection failed :'.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into ajiradb(Firstname, Lastname, Email, Password)
+        values(?, ?, ?, ?)");
+        $stmt->bind_param("ssss",$Firstname, $Lastname, $Email, $Password);
+        $stmt->execute();
+        echo "registration successful";
+        $stmt->close();
+        $conn->close();
+       }
 	?>
